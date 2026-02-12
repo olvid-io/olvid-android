5f29c9ab48a638c65346178c5ad0603f634145d4
Matthieu Finiasz <matthieu.finiasz@olvid.io>
2026-02-12 20:51:38 +0100

    Adjust build script

--- reproducible-builds/build.sh
+++ reproducible-builds/build.sh
@@ -1,9 +1,9 @@
-#!/bin/zsh
+#!/bin/bash
 
 set -e
 
 docker build -t olvid-android .
 
 
-echo '\n\nBuild successful, now cd.. and run:'
+echo 'Build successful, now cd.. and run:'
 echo 'docker run --rm -v "$(pwd)":/olvid -w /olvid/obv_messenger --user "$(id -u):$(id -g)" olvid-android ./gradlew assembleProdNogoogleRelease'
